a82b0fba0103e4abcc2f52f05506a37d3724b6f0
modeverv <modeverv@gmail.com>
2019-03-24 22:02:29 +0900

    Initial Commit

---
+++ Swift5TableView1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5TableView1
+//
+//  Created by seijiro on 2019/03/24.
+//  Copyright © 2019 norainu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
